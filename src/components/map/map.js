78e216f35de583d23358dc435f78b8bad780b282
Robin Murén <rur@qlik.com>
2019-05-02 13:50:18 +0200

    use qlik maps instead of google

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,5 +1,5 @@
+/* eslint-disable no-undef */
 import { LitElement, html } from 'lit-element';
-import * as mapStyles from './map-styles';
 
 import {
   // routeAmbulance,
@@ -8,7 +8,8 @@ import {
   firetrucks,
 } from './map-routes';
 
-window.initMap = () => { window.dispatchEvent(new CustomEvent('map-ready')); };
+window.onIdevioWebMapLoaded = () => { window.dispatchEvent(new CustomEvent('map-ready')); };
+const WEBMAP_KEY = 'placeholder';
 
 class CDMap extends LitElement {
   static get properties() {
@@ -21,25 +22,19 @@ class CDMap extends LitElement {
 
   constructor() {
     super();
-    this.APIKey = 'AIzaSyBJLqNRZNyt3_TgqDiBErIfamrAOdr-mf4';
+    this.qAPIKey = WEBMAP_KEY;
 
-    this.ambulancesArr = [];
-    this.fireTrucksArr = [];
-    this.drone = null;
     this.zoom = 12;
     this.lat = 45.2184704;
     this.lng = -75.7655448;
 
     window.addEventListener('map-ready', () => {
-      console.log('google maps ready');
       this.mapEl = this.shadowRoot.getElementById('map');
-      this.map = new google.maps.Map(this.mapEl, {
-        // new google.maps.LatLng(45.2424415, -75.7128212), // startLocation,
-        center: new google.maps.LatLng(this.lat, this.lng),
-        zoom: this.zoom,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true,
-        styles: mapStyles.default,
+      this.map = new idevio.map.WebMap({
+        div: this.mapEl,
+        baseMap: 'satellite_mercator',
+        center: [this.lat, this.lng],
+        zoomLevel: this.zoom,
       });
     });
   }
@@ -54,253 +49,243 @@ class CDMap extends LitElement {
           width: 100%;
           height: 100%;
         }
+        .idevio-attribution {
+          position: absolute;
+          overflow: hidden;
+          bottom: 3px;
+          right: 5px;
+          font: 8pt sans-serif;
+          pointer-events: none;
+          max-width: 80%;
+          color: black;
+          text-shadow: -1px -1px 0 #FFF, 1px -1px 0 #FFF, -1px 1px 0 #FFF, 1px 1px 0 #FFF;
+          padding: 2px;
+          bottom: 8px;
+        }
       </style>
-      <div id="map"></div>
+      <div id='map'></div>
     `;
   }
 
   firstUpdated() {
-    this.shadowRoot.appendChild(this._addScriptTag());
+    this.appendChild(this._addScriptTagQlik());
     super.firstUpdated();
   }
 
-  _addScriptTag() {
+  _addScriptTagQlik() {
     const scriptTag = document.createElement('script');
-    scriptTag.src = `https://maps.googleapis.com/maps/api/js?key=${this.APIKey}&libraries=places&callback=initMap`;
-    scriptTag.async = true;
-    scriptTag.defer = true;
+    scriptTag.src = `https://maps.qlikcloud.com/ravegeo/webmap5/script/webmap.nocache.js?key=${this.qAPIKey}&opt=tools,iconfactory`;
     return scriptTag;
   }
 
   // the smooth zoom function
-  _smoothZoom(max, cnt, timer = 80) {
-    if (max > cnt) {
-      const z = google.maps.event.addListener(this.map, 'zoom_changed', () => {
-        google.maps.event.removeListener(z);
-        this._smoothZoom(max, cnt + 1);
-      });
-      setTimeout(() => { this.map.setZoom(cnt); }, timer);
-    }
-    if (max < cnt) {
-      const z = google.maps.event.addListener(this.map, 'zoom_changed', () => {
-        google.maps.event.removeListener(z);
-        this._smoothZoom(max, cnt - 1);
-      });
-      setTimeout(() => { this.map.setZoom(cnt); }, timer);
-    }
+  _smoothZoom(factor) {
+    this.map.zoom(factor);
   }
 
   _panTo(lat, lng) {
-    this.map.panTo(new google.maps.LatLng(lat, lng));
+    this.map.moveTo([lat, lng], this.map.getResolution());
   }
 
-  _initFireTruck(firetruck) {
-    const lineSymbol = {
-      // path: firetruckSymbolPath,
-      path: google.maps.SymbolPath.CIRCLE,
-      scale: 10,
-      fillOpacity: 1,
-      fillColor: 'blue',
-      strokeColor: 'red',
-      strokeOpacity: 0.7,
-      strokeWeight: 6,
-    };
-    return new google.maps.Polyline({
-      path: firetrucks[firetruck].default,
-      icons: [{
-        icon: lineSymbol,
-        offset: '0%',
-      }],
-      strokeColor: 'rgba(255, 255, 0, 0)',
-      map: this.map,
-    });
-  }
+  _initAndAnimateDroneAct1() {
+    const myDataset = new idevio.map.MemoryDataset();
+    // eslint-disable-next-line no-unused-vars
+    const myLayer = new idevio.map.MarkerLayer(this.map, { name: 'Drone Marker', dataset: myDataset, pickable: true });
 
-  _initAmbulance(ambulance) {
-    const lineSymbol = {
-      path: google.maps.SymbolPath.CIRCLE,
-      scale: 10,
-      // scale: 0.05,
-      fillOpacity: 1,
-      fillColor: 'red',
-      strokeColor: 'white',
-      strokeOpacity: 0.7,
-      strokeWeight: 6,
-    };
-    return new google.maps.Polyline({
-      path: ambulances[ambulance].default,
-      icons: [{
-        icon: lineSymbol,
-        offset: '0%',
-      }],
-      strokeColor: 'rgba(255, 255, 0, 0)',
-      map: this.map,
-    });
-  }
+    const animationIcons = [];
+    for (let i = 0; i <= 10; i += 1) {
+      animationIcons.push(idevio.map.IconFactory.circle({ radius: 10, color: `rgba(0,0,0,${i / 10})` }));
+    }
 
-  _initDrone() {
-    const lineSymbol = {
-      path: google.maps.SymbolPath.CIRCLE, // droneSymbolPath,
-      scale: 4,
-      fillOpacity: 1,
-      fillColor: 'black',
-      strokeColor: 'black',
-      strokeOpacity: 0.7,
-      strokeWeight: 10,
-    };
-    return new google.maps.Polyline({
-      path: droneRoute,
-      icons: [{
-        icon: lineSymbol,
-        offset: '0%',
-      }],
-      strokeColor: 'rgba(255, 255, 0, 0)',
-      map: this.map,
-    });
-  }
-
-  _initDroneAct2() {
-    const droneMarker = new google.maps.Marker({ // last position from act 1
-      position: new google.maps.LatLng(45.2184704, -75.7655448),
-      icon: {
-        path: google.maps.SymbolPath.CIRCLE, // droneSymbolPath,
-        scale: 4,
-        fillOpacity: 1,
-        fillColor: 'black',
-        strokeColor: 'black',
-        strokeOpacity: 0.7,
-        strokeWeight: 10,
+    const pulseIcon = new idevio.map.Marker(myDataset, {
+      coordinate: [droneRoute[0].lat, droneRoute[0].lng],
+      style: {
+        icon: animationIcons[0],
       },
-      map: this.map,
     });
-    const icon = droneMarker.get('icon');
-    let type = 'decrease';
-    setInterval(() => {
-      if (type === 'decrease') {
-        icon.strokeOpacity = Math.round((icon.strokeOpacity - 0.1) * 10) / 10;
-        type = icon.strokeOpacity === 0 ? 'increase' : 'decrease';
-      } else {
-        icon.strokeOpacity = Math.round((icon.strokeOpacity + 0.1) * 10) / 10;
-        type = icon.strokeOpacity === 1 ? 'decrease' : 'increase';
+
+    const nonPulseIcon = new idevio.map.Marker(myDataset, {
+      coordinate: [droneRoute[0].lat, droneRoute[0].lng],
+      style: {
+        icon: idevio.map.IconFactory.circle({ radius: 5, color: 'rgba(0,0,0,1)' }),
+      },
+    });
+
+    this._fade(pulseIcon, animationIcons);
+    return this._move(droneRoute, [pulseIcon, nonPulseIcon], 80);
+  }
+
+  _blinkIcon(marker, speed, icons) {
+    let blink = true;
+    return setInterval(() => {
+      marker.setStyle({
+        icon: icons[blink ? 0 : 1],
+      });
+      blink = !blink;
+    }, speed);
+  }
+
+  _move(route, icons, speed) {
+    return new Promise((resolve) => {
+      let pos = 0;
+      const interval = setInterval(() => {
+        if (pos >= route.length) {
+          clearInterval(interval);
+          resolve();
+        } else {
+          icons.forEach((icon) => {
+            icon.setCoordinates([route[pos].lat, route[pos].lng]);
+          });
+          pos += 1;
+        }
+      }, speed);
+    });
+  }
+
+  _fade(marker, fadeIcons) {
+    let i = 0;
+    let grow = true;
+    return setInterval(() => {
+      if (i <= 0) {
+        grow = true;
+      } else if (i >= fadeIcons.length - 1) {
+        grow = false;
       }
-      droneMarker.set('icon', icon);
+
+      i = grow ? i + 1 : i - 1;
+
+      marker.setStyle({
+        icon: fadeIcons[i],
+      });
     }, 50);
-    return droneMarker;
+  }
+
+  // red & white
+  _initAndAnimateAmbulance(ambulance) {
+    const route = ambulances[ambulance].default;
+
+    const myDataset = new idevio.map.MemoryDataset();
+    // eslint-disable-next-line no-unused-vars
+    const myLayer = new idevio.map.MarkerLayer(this.map, { name: 'Ambulance Marker', dataset: myDataset, pickable: true });
+
+    const animationIcons = [];
+    animationIcons.push(idevio.map.IconFactory.circle({ radius: 10, color: 'rgba(255,0,0,1)' }));
+    animationIcons.push(idevio.map.IconFactory.circle({ radius: 10, color: 'rgba(255,255,255,1)' }));
+
+    const pulseIcon = new idevio.map.Marker(myDataset, {
+      coordinate: [route[0].lat, route[0].lng],
+      style: {
+        icon: animationIcons[0],
+      },
+    });
+
+    const nonPulseIcon = new idevio.map.Marker(myDataset, {
+      coordinate: [route[0].lat, route[0].lng],
+      style: {
+        icon: idevio.map.IconFactory.circle({ radius: 5, color: 'rgba(0,0,0,0.2)' }),
+      },
+    });
+
+    this._blinkIcon(pulseIcon, 600, animationIcons);
+    return this._move(route, [pulseIcon, nonPulseIcon], 20);
+  }
+
+  // red & yellow
+  _initAndAnimateFiretruck(firetruck) {
+    const route = firetrucks[firetruck].default;
+
+    const myDataset = new idevio.map.MemoryDataset();
+    // eslint-disable-next-line no-unused-vars
+    const myLayer = new idevio.map.MarkerLayer(this.map, { name: 'Firetruck Marker', dataset: myDataset, pickable: true });
+
+    const icons = [];
+    icons.push(idevio.map.IconFactory.circle({ radius: 10, color: 'rgba(0,0,255,1)' }));
+    icons.push(idevio.map.IconFactory.circle({ radius: 10, color: 'rgba(255,0,0,1)' }));
+    const pulseIcon = new idevio.map.Marker(myDataset, {
+      coordinate: [route[0].lat, route[0].lng],
+      style: {
+        icon: icons[0],
+      },
+    });
+
+    const nonPulseIcon = new idevio.map.Marker(myDataset, {
+      coordinate: [route[0].lat, route[0].lng],
+      style: {
+        icon: idevio.map.IconFactory.circle({ radius: 5, color: 'rgba(0,0,0,0.2)' }),
+      },
+    });
+
+    this._blinkIcon(pulseIcon, 1200, icons);
+    return this._move(route, [pulseIcon, nonPulseIcon], 20);
+  }
+
+  _initAndAnimateDroneAct2() {
+    const myDataset = new idevio.map.MemoryDataset();
+    // eslint-disable-next-line no-unused-vars
+    const myLayer = new idevio.map.MarkerLayer(this.map, { name: 'Drone Marker', dataset: myDataset, pickable: true });
+    const coord = [droneRoute[droneRoute.length - 1].lat, droneRoute[droneRoute.length - 1].lng];
+    const animationIcons = [];
+    for (let i = 0; i <= 10; i += 1) {
+      animationIcons.push(idevio.map.IconFactory.circle({ radius: 10, color: `rgba(0,0,0,${i / 10})` }));
+    }
+
+    const pulseIcon = new idevio.map.Marker(myDataset, {
+      coordinate: coord,
+      style: {
+        icon: animationIcons[0],
+      },
+    });
+
+    // eslint-disable-next-line no-unused-vars
+    const nonPulseIcon = new idevio.map.Marker(myDataset, {
+      coordinate: coord,
+      style: {
+        icon: idevio.map.IconFactory.circle({ radius: 5, color: 'rgba(0,0,0,1)' }),
+      },
+    });
+
+    this._fade(pulseIcon, animationIcons);
   }
 
   _initIncident() {
-    const incident = new google.maps.Marker({ // 45.2184704,-75.7655448
-      position: new google.maps.LatLng(45.2184704, -75.7655448),
-      icon: './assets/incident.svg',
-      map: this.map,
+    const myDataset = new idevio.map.MemoryDataset();
+    const myLayer = new idevio.map.MarkerLayer(this.map, {
+      name: 'Position Marker',
+      dataset: myDataset,
+      pickable: true,
     });
+
+    // a predefined symbol
+    const incidentIcon = new idevio.map.Icon({
+      url: './assets/incident.svg',
+      anchorX: 'LEFT',
+      anchorY: 'BOTTOM',
+    });
+
+    // eslint-disable-next-line no-unused-vars
+    const incident = new idevio.map.Marker(myDataset, {
+      coordinate: [45.2184704, -75.7655448],
+      style: { icon: incidentIcon },
+    });
+
     let blinking = false;
     setInterval(() => {
-      if (blinking) {
-        incident.setMap(null);
-      } else {
-        incident.setMap(this.map);
-      }
+      myLayer.setVisible(blinking);
       blinking = !blinking;
     }, 600);
   }
 
-  _animateVehicule(line, blink, ms = 1000) {
-    function animateCircle(l) {
-      return new Promise((resolve) => {
-        let count = 0;
-        const intervalId = setInterval(() => {
-          count = (count + 1) % 200;
-          if (count >= 199) {
-            clearInterval(intervalId);
-            resolve();
-          }
-          const icons = l.get('icons');
-          icons[0].offset = `${count / 2}%`;
-          l.set('icons', icons);
-        }, 60);
-      });
-    }
-    function blinking(l) {
-      setInterval(() => {
-        const icons = l.get('icons');
-        const fillColor = icons[0].icon.strokeColor;
-        const strokeColor = icons[0].icon.fillColor;
-        icons[0].icon.fillColor = fillColor;
-        icons[0].icon.strokeColor = strokeColor;
-        l.set('icons', icons);
-      }, ms);
-    }
-    if (blink) { blinking(line, ms); }
-    return animateCircle(line);
-  }
-
-  _animateDrone(line) {
-    function animate(l) {
-      return new Promise((resolve) => {
-        let count = 0;
-        const intervalId = setInterval(() => {
-          count = (count + 1) % 200;
-          if (count >= 199) {
-            clearInterval(intervalId);
-            resolve();
-          }
-          const icons = l.get('icons');
-          icons[0].offset = `${count / 2}%`;
-          l.set('icons', icons);
-        }, 30);
-      });
-    }
-    function fade(l) {
-      const icons = l.get('icons');
-      let type = 'decrease';
-      setInterval(() => {
-        if (type === 'decrease') {
-          icons[0].icon.strokeOpacity = Math.round((icons[0].icon.strokeOpacity - 0.1) * 10) / 10;
-          type = icons[0].icon.strokeOpacity === 0 ? 'increase' : 'decrease';
-        } else {
-          icons[0].icon.strokeOpacity = Math.round((icons[0].icon.strokeOpacity + 0.1) * 10) / 10;
-          type = icons[0].icon.strokeOpacity === 1 ? 'decrease' : 'increase';
-        }
-        l.set('icons', icons);
-      }, 50);
-    }
-    fade(line);
-    return animate(line);
-  }
-
   getNearestHospitals() {
-    return new Promise((resolve) => {
-      const placeService = new google.maps.places.PlacesService(this.map);
-      const request = {
-        location: new google.maps.LatLng(45.2184704, -75.7655448), // last position of the drone
-        keyword: 'hospital',
-        rankBy: google.maps.places.RankBy.DISTANCE,
-        type: ['hospital'/* , 'police', 'fire_station' */], // supported types can be found here -> https://developers.google.com/places/supported_types
-      };
-      placeService.nearbySearch(request, (res) => {
-        resolve(res);
-      });
-    });
-  }
-
-  startAnimation() {
-    this.ambulancesArr.forEach((ambulance) => {
-      this._animateVehicule(ambulance, true);
-    });
-    this.fireTrucksArr.forEach((fireTruck) => {
-      this._animateVehicule(fireTruck, true, 500);
-    });
-    return this._animateDrone(this.drone).then(() => {
-      this._smoothZoom(17, this.map.getZoom());
-      const videoEvent = new CustomEvent('playVideo', {
-        detail: {
-          type: 'video/webm',
-          src: './assets/Act_1-labelled.test9.webm',
-        },
-      });
-      window.dispatchEvent(videoEvent);
-    });
+    // Patric Nordström looked this up and found other options. The previous
+    // "closest" hospitals included minor health clinics and similar that
+    // wouldn't normally accept ambulances and such. (No emergency room)
+    return [
+      { name: 'Montfort Hospital', lat: 45.446375, lng: -75.638164 },
+      { name: 'Queensway-Carleton Hospital', lat: 45.335536, lng: -75.808361 },
+      { name: 'Children\'s Hospital of Eastern Ontario (CHEO)', lat: 45.400882, lng: -75.65225 },
+      { name: 'The Ottawa Hospital Civic Campus', lat: 45.392345, lng: -75.720532 },
+      { name: 'The Ottawa Hospital General Campus', lat: 45.4033, lng: -75.648996 },
+    ];
   }
 }
 
